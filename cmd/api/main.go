cf68162fa4f364dd63b58bc7cb1560b63e50292e
Prachiti Kitey <prachitikitey86@gmail.com>
2026-07-16 21:33:47 +0530

    env setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/Prachiti27/go-backend/internal/env"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8081"),
 	}
 
 	app := &application{
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Prachiti27/go-backend
 
 go 1.26.1
 
-require github.com/go-chi/chi/v5 v5.3.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
